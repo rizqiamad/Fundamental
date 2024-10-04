2f70a8917d64fefe3736f8344a61971c2627ffe0
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-04 11:06:26 +0700

    array day

--- README.md
+++
@@ -1,3 +0,0 @@
-# Fundamental
-this is first day in learning git version control  
-you have to learn seriously to get the best return.
---
+++ builtInMethod.ts
@@ -0,0 +1,9 @@
+let arr:string[] = ['Apple', 'Manggo', 'Durian'];
+
+console.log(arr.toString());
+console.log(arr.includes('Apple'));
+
+let num:number = 1
+if (num) {
+    console.log('True');
+}
