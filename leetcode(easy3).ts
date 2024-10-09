910699c85c009b920bc9918df5833a3b13afe674
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-09 14:16:35 +0700

    roman to int

---
+++ leetcode(easy3).ts
@@ -0,0 +1,17 @@
+// function romanToInt(s: string): number {
+//     // varibel output
+//     let res:number = 0
+//     let romanMap: Record<string, any> = {
+//         1: "I",
+//         5: "V",
+//         10: "X",
+//         50: "L",
+//         100: "C",
+//         500: "D",
+//         1000: "M",
+//       };
+
+//     return res
+// };
+
+// console.log(romanToInt('XC'));
