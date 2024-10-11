71f185e5697dc1f48624c5fbe462a4230d3a4c2b
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-11 10:51:25 +0700

    linked list

---
+++ linked.ts
@@ -0,0 +1,57 @@
+class Node {
+    element: number
+    next: any
+
+    constructor(element: number) {
+        this.element = element
+        this.next = null
+    }
+}
+
+class LinkedList {
+    head: Node | null
+    size: number
+
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    add(element: number){
+        let node = new Node(element)
+
+        let current: any
+
+        if (this.head === null) {
+            this.head = node
+        }else{
+            current = this.head
+
+            while (current.next) {
+                current = current.next
+            }
+
+            current.next = node
+        }
+
+        this.size += 1
+    }
+
+    printList(){
+        let curr:any = this.head
+        while(curr){
+            console.log(curr.element)
+            curr = curr.next
+        }
+    }
+}
+
+const linkedList = new LinkedList()
+
+linkedList.add(1)
+linkedList.add(2)
+linkedList.add(3)
+linkedList.add(4)
+
+console.log(linkedList.head?.next);
+linkedList.printList()
