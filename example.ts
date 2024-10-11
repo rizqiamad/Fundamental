436dbd70d41ea5ba3a97ae7e34aadb162cd4d228
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-11 09:45:15 +0700

    stack

---
+++ example.ts
@@ -0,0 +1,12 @@
+class example{
+    #name:string
+
+    constructor(name:string, age:number) {
+        this.#name = name
+        
+    }
+    
+}
+
+const hanif = new example('hanif', 19)
+const naufal = new example('naufal', 19)
