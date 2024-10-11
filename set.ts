9a089e493c983dd8072622dc3d098fd73b5bbb19
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-11 10:15:20 +0700

    set

---
+++ set.ts
@@ -0,0 +1,15 @@
+const fruits: string[] = ['banana', 'apple', 'jackfruit', 'apple']
+
+const setFruits = new Set(fruits)
+
+setFruits.add('apple')
+
+console.log(setFruits.values());
+
+setFruits.forEach(item => {
+    console.log(item);
+})
+
+setFruits.clear()
+
+console.log(setFruits);
