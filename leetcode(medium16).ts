047e922540ce1f516adddb420c22394878303b0f
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-16 14:41:20 +0700

    medium 16

--- leetcode(medium16).ts
+++ leetcode(medium16).ts
@@ -1,12 +1,32 @@
 function subsets(nums: number[]): number[][] {
-    let res: number[][] = []
-    res.push([])
+  let result: number[][] = [];
+  let res: number[];
+  result.push([]);
 
-    if (nums.length === 0) return res
+  if (nums.length === 0) return result;
 
-    nums.forEach((item, _idx, arr) => item)
+  nums.sort((a, b) => a - b);
 
-    return res
-};  
+  for (let i = 0; i < nums.length; i++) {
+    res = [];
+    res.push(nums[i]);
+    result.push([...res]);
+    for (let j = i + 1; j < nums.length; j++) {
+      res.push(nums[j]);
+      if (res.length >= 3) {
+        result.push([res[0], res[res.length - 1]]);
+        if (res.length > 3) {
+          let temp = res.slice(1, res.length - 1);
+          for (let k = 0; k < temp.length; k++) {
+            result.push([res[0], temp[k], res[res.length - 1]]);
+          }
+        }
+      }
+      result.push([...res]);
+    }
+  }
 
-console.log(subsets([1,2,3]))
+  return result;
+}
+
+console.log(subsets([1, 2, 3, 4]));
