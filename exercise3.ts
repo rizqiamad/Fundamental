7c8a26cd46352fbf82840e8747645fd3826c34bf
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-16 09:22:59 +0700

    pascal triangle

---
+++ exercise3.ts
@@ -0,0 +1,18 @@
+function pascalTriangle(n: number): number[][] {
+  let res: number[][] = [];
+
+  res.push([1]);
+  for (let i = 0; i < n - 1; i++) {
+    let temp: number[] = [];
+    for (let j = -1; j < res.length; j++) {
+      if (res[i][j] === undefined) temp.push(0 + res[i][j + 1]);
+      else if (res[i][j + 1] === undefined) temp.push(res[i][j] + 0);
+      else temp.push(res[i][j] + res[i][j + 1]);
+    }
+    res.push(temp);
+  }
+
+  return res;
+}
+
+console.log(pascalTriangle(5))
